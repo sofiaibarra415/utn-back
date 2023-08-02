077f0e5cc654208e56979358d55a2c0706208313
sofiaibarra415 <127771748+sofiaibarra415@users.noreply.github.com>
2023-08-01 22:33:07 -0300

    Clase 01/08

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -16,4 +16,25 @@ async function insertNovedad (obj) {
       }
     } 
 
-module.exports = { getNovedades, insertNovedad }
+    async function deleteNovedadesById(id){
+      var query = 'delete from novedades where id=?';
+      var rows = await pool.query (query, [id]);
+      return rows;
+    }
+    async function getNovedadById(id){
+      var query = "select * from novedades where id=?";
+      var rows = await pool.query (query, [id]);
+      return rows[0];
+    }
+
+    async function modificarNovedadById(obj, id){
+      try{
+        var query = "update novedades set ? where id = ?";
+        var rows = await pool.query(query,[obj, id]);
+        return rows;
+      } catch (error){
+        throw error;
+      }
+    }
+
+module.exports = { getNovedades, insertNovedad, deleteNovedadesById, getNovedadById, modificarNovedadById}
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,43 +1,89 @@
 var express = require('express');
 var router = express.Router();
-var novedadesModel = require ('../../models/novedadesModel')
+var novedadesModel = require('../../models/novedadesModel')
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   var novedades = await novedadesModel.getNovedades();
+  novedades.sort((a, b) => a.id - b.id);
   res.render('admin/novedades', {
-    layout:'admin/layout' ,
+    layout: 'admin/layout',
     persona: req.session.nombre,
     novedades
   });
 });
-router.get('/agregar', (req, res, next)=>{
+router.get('/agregar', (req, res, next) => {
   res.render('admin/agregar', {
     layout: 'admin/layout'
   })
 });
-router.post('/agregar', async(req, res, next) =>{
+router.post('/agregar', async (req, res, next) => {
 
-  try{
+  try {
     console.log(req.body)
 
-    if (req.body.titulo !="" && req.body.subtitulo !="" && req.body.cuerpo !=""){
+    if (req.body.nombre != "" &&  req.body.testimonio != "") {
       await novedadesModel.insertNovedad(req.body);
       res.redirect('/admin/novedades')
-    }else {
+    } else {
       res.render('admin/agregar', {
-        layout:'admin/layout',
-        error:true,
-        message:'Todos los campos son requeridos'
+        layout: 'admin/layout',
+        error: true,
+        message: 'Todos los campos son requeridos'
       })
     }
-  }catch (error){
+  } catch (error) {
     console.log(error)
-    res.render('admin/agregar',{
-      layout:'admin/layout',
-      error:true,
-      message:'No se cargo la novedad'
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se cargo la novedad'
+    })
+  }
+});
+
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades');
+});
+
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  var novedad = await novedadesModel.getNovedadById(id);
+
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  });
+});
+
+router.post('/modificar', async (req, res, next) => {
+  try {
+    const novedadEnDB = await novedadesModel.getNovedadById(req.body.id)
+    const body = req.body
+    if (body.nombre !== novedadEnDB.nombre || body.testimonio !== novedadEnDB.testimonio) {
+      var obj = {
+        nombre: req.body.nombre,
+        testimonio: req.body.testimonio
+      }
+
+      await novedadesModel.modificarNovedadById(obj, req.body.id);
+      res.redirect('/admin/novedades');
+    } else {
+      res.render('admin/modificar', {
+        layout: 'admin/layout',
+        error: true, message: 'No se modificó la novedad'
+      })
+    }
+
+  } catch (error) {
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se modificó la novedad'
     })
   }
 })
+
+
 module.exports = router;
