f16bc190e9fd1df918ed2afd718f1590791c31e5
Ezequiel Agustin Ibarra <82052522+ezeagusibarra001@users.noreply.github.com>
2023-08-11 19:01:01 -0300

    fix novedades

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var novedadesModel = require('./../models/novedadesModel');
-var cloudinary = require('cloudinary').v2;
+var novedadesModel = require("./../models/novedadesModel");
+var cloudinary = require("cloudinary").v2;
 
-router.get('/novedades', async function (req, res, next){
-    let novedades = await novedadesModel.getNovedades();
-
-    novedades = novedades.map(novedades =>{
-        if (novedades.img_id){
-            const imagen = cloudinary.url(novedades.img_id,{
-                width: 960,
-                height:200,
-                crop: 'fill'
-            });
-            return {
-                ...novedades,
-                imagen: ''
-            }
-        }
-    });
-    res.json(novedades);
+router.get("/novedades", async function (req, res, next) {
+  let novedades = await novedadesModel.getNovedades();
+  res.json(novedades);
 });
 module.exports = router;
