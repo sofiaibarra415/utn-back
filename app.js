bf08e7fcf70752de905019b2951863d050518ad7
sofiaibarra415 <127771748+sofiaibarra415@users.noreply.github.com>
2023-08-10 22:52:15 -0300

    clase 10/08

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var fileUpload = require ('express-fileupload');
+var cors = require ('cors');
 
 require('dotenv').config();
 var session = require ('express-session');
@@ -11,6 +12,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
 var adminRouter = require('./routes/admin/novedades');
+var apiRouter = require('./routes/api');
+
 var app = express();
 
 // view engine setup
@@ -22,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(session({
   secret: '1234567jujuju',
@@ -41,11 +45,16 @@ secured = async (req, res, next) => {
     console.log(error);
   }
 }
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
 app.use('/admin/novedades',secured, adminRouter);
+app.use('/api', cors(), apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var novedadesModel = require('../../models/novedadesModel')
+var novedadesModel = require('../../models/novedadesModel');
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.image(novedad.img_id, {
+        width: 100,
+        height: 100,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: ''
+      }
+    }
+  });
+
   novedades.sort((a, b) => a.id - b.id);
   res.render('admin/novedades', {
     layout: 'admin/layout',
@@ -20,10 +44,17 @@ router.get('/agregar', (req, res, next) => {
 router.post('/agregar', async (req, res, next) => {
 
   try {
-    console.log(req.body)
+    var img_id = '';
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    }
 
-    if (req.body.nombre != "" &&  req.body.testimonio != "") {
-      await novedadesModel.insertNovedad(req.body);
+    if (req.body.nombre != "" && req.body.testimonio != "") {
+      await novedadesModel.insertNovedad({
+        ...req.body,
+        img_id
+      });
       res.redirect('/admin/novedades')
     } else {
       res.render('admin/agregar', {
@@ -37,13 +68,17 @@ router.post('/agregar', async (req, res, next) => {
     res.render('admin/agregar', {
       layout: 'admin/layout',
       error: true,
-      message: 'No se cargo la novedad'
+      message: 'No se cargó el testimonio'
     })
   }
 });
 
 router.get('/eliminar/:id', async (req, res, next) => {
   var id = req.params.id;
+  let novedad = await novedadesModel.getNovedadById(id);
+  if (novedad.img_id) {
+    await (destroy(novedad.img_id));
+  }
   await novedadesModel.deleteNovedadesById(id);
   res.redirect('/admin/novedades');
 });
@@ -60,12 +95,80 @@ router.get('/modificar/:id', async (req, res, next) => {
 
 router.post('/modificar', async (req, res, next) => {
   try {
+    let img_id = req.body.img_original;
+    let borrar_img_vieja = false;
+    if (req.body.img_delete === "1") {
+      img_id = null;
+      borrar_img_vieja = true;
+    } else {
+      if (req.files) {
+        imagen = req.files.imagen;
+        img_id = (await
+          uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
+      }
+    }
+    if (borrar_img_vieja && req.body.img_original) {
+      await (destroy(req.body.img_original));
+    }
     const novedadEnDB = await novedadesModel.getNovedadById(req.body.id)
     const body = req.body
-    if (body.nombre !== novedadEnDB.nombre || body.testimonio !== novedadEnDB.testimonio) {
+    if (body.nombre !== novedadEnDB.nombre || body.testimonio !== novedadEnDB.testimonio || body.img_id !== novedadEnDB.img_id) {
       var obj = {
         nombre: req.body.nombre,
-        testimonio: req.body.testimonio
+        testimonio: req.body.testimonio,
+        img_id
+      }
+
+      await novedadesModel.modificarNovedadById(obj, req.body.id);
+      res.redirect('/admin/novedades');
+    } else {
+      console.log("nashee")
+      res.render('admin/modificar', {
+        layout: 'admin/layout',
+        error: true, message: 'No se modificó el testimonio'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se modificó el testimoio'
+    })
+  }
+})
+
+
+module.exports = router;
+
+
+
+
+/*try {
+    let img_id = req.body.img_original;
+    let borrar_img_vieja = false;
+    if (req.body.img_delete === "1") {
+      img_id = null;
+      borrar_img_vieja = true;
+    } else {
+      if (req.files && Object.keys(req.files).lenght > 0) {
+        imagen = req.files.imagen;
+        img_id = (await
+          uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
+      }
+    }
+    if (borrar_img_vieja && req.body.img_original) {
+      await (destroy(req.body.img_original));
+    }
+
+    const novedadEnDB = await novedadesModel.getNovedadById(req.body.id)
+    const body = req.body
+    if (body.nombre !== novedadEnDB.nombre || body.testimonio !== novedadEnDB.testimonio || body.img_id !== novedadEnDB.img_id) {
+      var obj = {
+        nombre: req.body.nombre,
+        testimonio: req.body.testimonio,
+        img_id
       }
 
       await novedadesModel.modificarNovedadById(obj, req.body.id);
@@ -73,17 +176,12 @@ router.post('/modificar', async (req, res, next) => {
     } else {
       res.render('admin/modificar', {
         layout: 'admin/layout',
-        error: true, message: 'No se modificó la novedad'
+        error: true, message: 'No se modificó el testimonio'
       })
     }
-
   } catch (error) {
     res.render('admin/modificar', {
       layout: 'admin/layout',
-      error: true, message: 'No se modificó la novedad'
+      error: true, message: 'No se modificó el testimoio'
     })
-  }
-})
-
-
-module.exports = router;
+  }*/
